8217f7bdc848ac34587d43ba2c5828e6257968ab
filiptepper <filip@tepper.pl>
2010-09-23 12:09:31 +0200

    added basic config

--- readmio.js
+++ readmio.js
@@ -1,7 +1,22 @@
-var readmio = require("express").createServer();
+var express = require("express");
+var fs = require("fs");
+var yaml = require("yaml");
 
-readmio.get("/", function(request, response) {
-  response.send("Hello world!");
+var config = {};
+
+var application = {};
+application.run_server = function() {
+  var readmio = express.createServer();
+
+  readmio.get("/", function(request, response) {
+    response.send("Hello world!");
+  });
+
+  readmio.listen(config["application"]["port"]);
+}
+
+fs.readFile("./config/config.yml", "UTF-8", function(error, data) {
+  if (error) throw error;
+  config = yaml.eval(data);
+  application.run_server();
 });
-
-readmio.listen(3000);
