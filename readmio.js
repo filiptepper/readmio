f37e85b6b1ebfbab3a14636530a1c1ba09994674
filiptepper <filip@tepper.pl>
2010-09-28 15:44:54 +0200

    hacking on oauth

--- readmio.js
+++ readmio.js
@@ -5,8 +5,9 @@ var OAuth            = require("oauth").OAuth;
 var yaml             = require("yaml");
 
 var config = {};
-
 var application = {};
+var authentication = {};
+
 application.run_server = function() {
   var readmio = express.createServer();
 
@@ -14,22 +15,35 @@ application.run_server = function() {
     response.render("./desktop/index.jade");
   });
 
+  readmio.get("/oauth/authenticate", function() {
+    authentication.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+      console.log(results);
+      authentication.getOAuthAccessToken(oauth_token, oauth_token_secret, function(error, oauth_access_token, oauth_access_token_secret, results2) {
+        console.log(results2);
+        authentication.getProtectedResource("http://api.twitter.com/1/statuses/home_timeline.json", "GET", oauth_access_token, oauth_access_token_secret, function(error, data, response) {
+          console.log(data);
+        });
+      });
+    });
+  });
+
   readmio.set("view engine", "jade");
   readmio.set("view options", { layout: false });
 
   readmio.listen(config["application"]["port"]);
+}
 
-  var authentication = new OAuth("http://twitter.com/oauth/request_token",
+fs.readFile("./config/config.yml", "UTF-8", function(error, data) {
+  if (error) throw error;
+  config = yaml.eval(data);
+
+  authentication = new OAuth("http://twitter.com/oauth/request_token",
     "http://twitter.com/oauth/access_token",
     config["twitter"]["key"],
     config["twitter"]["secret"],
     "1.0",
     config["twitter"]["callback_url"],
     "HMAC-SHA1");
-}
 
-fs.readFile("./config/config.yml", "UTF-8", function(error, data) {
-  if (error) throw error;
-  config = yaml.eval(data);
   application.run_server();
 });
